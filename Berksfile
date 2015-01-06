aba2ec77420820a37c1ac820822ea1040a463eb7
Suftin, Ivan <isuftin@usgs.gov>
2015-01-06 10:35:21 -0600

    Psi-probe deployed

--- Berksfile
+++ Berksfile
@@ -1,3 +1,6 @@
 source "https://supermarket.getchef.com"
 
 metadata
+
+cookbook "stig", :git => "https://github.com/USGS-WSI-COOKBOOKS/stig.git"
+cookbook "wsi_tomcat", :git => "https://github.com/USGS-WSI-COOKBOOKS/wsi_tomcat.git"
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'cida_psiprobe'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer       'Ivan Suftin'
+maintainer_email 'isuftin@usgs.gov'
 license          'gplv3'
-description      'Installs/Configures cida_psiprobe'
+description      'Deploys Psi-Probe to a CIDA Tomcat server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'wsi_tomcat', '~> 0.1.0'
