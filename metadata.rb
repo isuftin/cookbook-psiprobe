7b277298b830a6b9571e5535a498617246484ff5
Suftin, Ivan <isuftin@usgs.gov>
2015-01-06 12:06:07 -0600

    Updated metadata.rb and kitchen.yml

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,46 @@ license          'gplv3'
 description      'Deploys Psi-Probe to a CIDA Tomcat server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-source_url       'https://github.com/USGS-CIDA/cookbook-psiprobe'
+supports         'centos'
 
+depends "stig", '~> 0.1.0'
+depends "wsi_tomcat", '~> 0.1.0'
 
-depends 'wsi_tomcat', '~> 0.1.0'
+recipe "cida_psiprobe::default", "Includes the recipe to the WSI Tomcat cookbook to deploy the application"
+
+attribute "wsi_tomcat",
+  :display_name => "WSI Tomcat Options",
+  :type => "hash",
+  :required => "required"
+  
+attribute "wsi_tomcat/instances",
+  :display_name => "WSI Tomcat Instances",
+  :type => "hash",
+  :required => "required"
+  
+attribute "wsi_tomcat/instances/instance",
+  :display_name => "WSI Tomcat Instance",
+  :description => "The name of a tomcat instance to deploy to. (e.g., default)",
+  :required => "required"
+  
+attribute "wsi_tomcat/instances/instance/application",
+  :display_name => "WSI Tomcat Instance Application",
+  :type => "hash",
+  :required => "required"
+  
+attribute "wsi_tomcat/instances/instance/application/psiprobe",
+  :display_name => "PSI-Probe Application",
+  :type => "hash",
+  :required => "required"
+  
+attribute "wsi_tomcat/instances/instance/application/psiprobe/url",
+  :display_name => "PSI-Probe Application Artifact URL",
+  :type => "string",
+  :required => "required"
+  
+attribute "wsi_tomcat/instances/instance/application/psiprobe/url",
+  :display_name => "PSI-Probe Application Artifact final name",
+  :type => "string",
+  :required => "required",
+  :default => "psi-probe"
+  
