4d97fa152c0021d8fbd871b2e7f5efe9c6e7a454
Suftin, Ivan <isuftin@usgs.gov>
2015-01-06 11:20:47 -0600

    Foodcritic fixes and some metadata.rb updates

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default[:wsi_tomcat][:instances][:default][:application][:psiprobe][:url]                 = 'http://cida.usgs.gov/maven/cida-public-thirdparty/com/googlecode/psiprobe/web/2.3.3/web-2.3.3.war'
-default[:wsi_tomcat][:instances][:default][:application][:psiprobe][:final_name]          = 'psi-probe'
+default["wsi_tomcat"]["instances"]["default"]["application"]["psiprobe"]["url"]                 = 'http://cida.usgs.gov/maven/cida-public-thirdparty/com/googlecode/psiprobe/web/2.3.3/web-2.3.3.war'
+default["wsi_tomcat"]["instances"]["default"]["application"]["psiprobe"]["final_name"]          = 'psi-probe'
--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,7 @@ license          'gplv3'
 description      'Deploys Psi-Probe to a CIDA Tomcat server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+source_url       'https://github.com/USGS-CIDA/cookbook-psiprobe'
+
 
 depends 'wsi_tomcat', '~> 0.1.0'
